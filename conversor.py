bb9a7a0d5f3fd80688b10cd2916b386d26b3f392
kaiky <kaiky.santos.lima@escola.pr.gov.br>
2026-03-11 11:37:54 -0300

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,10 @@
+# Solicita temperatura em graus Celsius ao usuário
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+
+# Calcula a temperatura em graus Fahrenheit
+fahrenheit = _________________
+
+
+# Exibe a conversão
+print(f"{celsius} graus Celsius equivalem a {fahrenheit} graus Fahrenheit.")
+
