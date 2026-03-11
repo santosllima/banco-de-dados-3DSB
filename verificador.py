030fd72cc1ce924c4c823439ebc2a5f59e8cd527
kaiky <kaiky.santos.lima@escola.pr.gov.br>
2026-03-11 11:36:46 -0300

    Add files via upload

---
+++ verificador.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário que digite um número inteiro
+numero = int(input("__________________________: "))
+
+# Verifica se o número é par
+if numero % 2 == 0:
+    # Se for par, imprime a mensagem
+    print(f"{numero} é par.")
+else:
+    # Se for ímpar, imprime a mensagem
+    print(f"{numero} é ______________.")
